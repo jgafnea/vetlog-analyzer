95a87fff6dc06ad771c119e55b14ae644780ffe2
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-11-18 23:13:11 -0600

    feat: add docker, compose, and init.sql

---
+++ docker/init.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE IF NOT EXISTS `vetlog`;
+USE `vetlog`;
+
+-- User table
+CREATE TABLE user(
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    account_non_expired bit(1) NOT NULL,
+    account_non_locked bit(1) NOT NULL,
+    credentials_non_expired bit(1) NOT NULL,
+    date_created datetime NOT NULL,
+    email varchar(255) DEFAULT NULL,
+    enabled bit(1) NOT NULL,
+    first_name varchar(255) DEFAULT NULL,
+    last_name varchar(255) DEFAULT NULL,
+    mobile varchar(255) DEFAULT NULL,
+    password varchar(255) NOT NULL,
+    role varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Breed table
+CREATE TABLE breed(
+    id bigint NOT NULL AUTO_INCREMENT,
+    date_created datetime(6) NOT NULL,
+    name varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Pet table
+CREATE TABLE pet(
+    id bigint NOT NULL AUTO_INCREMENT,
+    birth_date datetime(6) NOT NULL,
+    date_created datetime(6) NOT NULL,
+    dewormed bit(1) NULL,
+    name varchar(255) NOT NULL,
+    status varchar(255) NOT NULL,
+    sterilized bit(1) NULL,
+    uuid varchar(255) NOT NULL,
+    vaccinated bit(1) NULL,
+    adopter_id bigint DEFAULT NULL,
+    breed_id bigint DEFAULT NULL,
+    user_id bigint DEFAULT NULL,
+    pet_id bigint DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Vaccination table
+CREATE TABLE vaccination(
+    id bigint NOT NULL AUTO_INCREMENT,
+    date date NOT NULL,
+    name varchar(255) NOT NULL,
+    status enum('APPLIED','PENDING') NOT NULL,
+    pet_id bigint DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Data
+INSERT INTO user VALUES (1, 1, 1, 1, '2024-04-21 20:08:51', 'contact@josdem.io', 1, 'jose', 'morales', NULL, 'password', 'USER', 'josdem');
+INSERT INTO breed VALUES (1, '2022-06-03 21:23:01.000000', 'Chihuahua', 'DOG');
+INSERT INTO pet VALUES (1, '2023-05-14 20:59:00.000000', '2023-10-23 10:24:18.375559', NULL, 'Cremita', 'OWNED', NULL, '0694c042-a4ae-4f99-b329-98f519da0164', NULL, NULL, 1, 1, NULL);
+
+-- Permissions (updated for container networking)
+CREATE USER 'vetlog_user'@'%' IDENTIFIED BY 'vetlog_password';
+GRANT ALL PRIVILEGES ON vetlog.* TO 'vetlog_user'@'%';
+FLUSH PRIVILEGES;
